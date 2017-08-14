8f0978b90c5bee7ddfce24ea54ff5d6eacb2dd1b
skillet123q@gmail.com <WinstoNNN95>
2017-08-14 16:38:17 +0300

    add admin controller

---
+++ controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+use yii\web\Controller;
+
+/**
+ * Developer: skillet123q@gmail.com
+ * Date: 14.08.2017
+ * Time: 16:37
+ */
+class AdminController extends Controller
+{
+
+    public function actionIndex()
+    {
+        return "ok!";
+    }
+}
