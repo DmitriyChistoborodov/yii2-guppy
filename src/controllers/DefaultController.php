cd606dbeac4bd8c758bc7ff9d290a00d3bf78ccd
skillet123q@gmail.com <WinstoNNN95>
2017-08-30 15:18:13 +0300

    Add migrations, new models with ActiveQuery and new controllers

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -1,7 +1,11 @@
 <?php
 namespace xbrodies\guppy\controllers;
 
+use xbrodies\guppy\models\Page;
+use Yii;
 use yii\web\Controller;
+use yii\web\Request;
+use yii\web\Response;
 
 /**
  * Developer: skillet123q@gmail.com
@@ -12,6 +16,12 @@ class DefaultController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render("index.twig");
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization");
+        $pages = Page::find()->asArray(true)->all();
+
+        return $pages;
     }
 }
---
+++ src/controllers/ResourceController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Developer: skillet123q@gmail.com
+ * Date: 30.08.2017
+ * Time: 15:02
+ */
+
+namespace xbrodies\guppy\controllers;
+
+
+use yii\web\Controller;
+
+class ResourceController extends Controller
+{
+    public function actionTree()
+    {
+
+    }
+}
